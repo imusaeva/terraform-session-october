0af91f7a324c38d0951deedb7b07ebeb9b32bcda
EC2 Default User <ec2-user@ip-172-31-26-246.us-west-2.compute.internal>
2022-11-02 00:41:58 +0000

    modified session-10

--- session-10/main.tf
+++ session-10/main.tf
@@ -1,9 +1,11 @@
-module "ec2" {
-    source = "github.com/imusaeva/terraform-session-october//modules/ec2"
-    env = "dev"
-    instance_type = "t2.micro"
-    ami = "ami-09d3b3274b6c5d4aa"
-}
+####### calling module from our github repo ######
+# module "ec2" {
+    # source = "github.com/imusaeva/terraform-session-october//modules/ec2?ref=v1.0.0"
+    # env = "dev"
+    # instance_type = "t2.micro"
+    # ami = "ami-09d3b3274b6c5d4aa"
+# }
+
 # github.com = main domain
 # imusaeva = Organization
 # terraform-session-october = repository
@@ -11,3 +13,9 @@ module "ec2" {
 # main = branch
 # modules = directory
 # ec2 = subdirectory
+
+########## calling modules from terraform registry.io #######
+module "vpc_example_simple-vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+  version = "3.18.1"
+}
